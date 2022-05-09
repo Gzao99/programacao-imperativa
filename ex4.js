b009daf7192ed76b648affb1f40ad041e25f099e
Guilherme <guilhermesrocha99@gmail.com>
2022-05-09 20:47:34 -0300

    Adicionando exercicio

---
+++ ex4.js
@@ -0,0 +1,8 @@
+const nome = "José"
+let idade = 27
+let peso = 83.5
+let altura = 1.70
+let plano = true
+const imc =(peso / (altura * altura)).toFixed(2);
+
+console.log(nome, " tem ", idade, " anos e seu índice de massa corporal é de ", imc,"%" )
